b671a3d0cd1cf1c0d8fc66f3f744845848dbf195
José David Medina Paredes <cosoacount@gmail.com>
2024-11-18 20:11:07 -0600

    Init project

---
+++ src/main/java/chat/Constants.java
@@ -0,0 +1,7 @@
+package chat;
+
+public class Constants {
+    public static final String ADDR = "230.1.1.1";
+    public static final int PORT = 4000;
+    public static final int BUF_LEN = 2048;
+}
